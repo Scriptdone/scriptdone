9a7b2b451918be564328bf0f57796cd4766f6bb2
Ritik <scriptdone.software@gmail.com>
2025-07-14 23:27:32 +0530

    changes

--- footer.php
+++ footer.php
@@ -100,6 +100,44 @@
 
   <script src="assets/js/main.js"></script>
 
+  <script>
+                var url = 'https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?48452';
+                var s = document.createElement('script');
+                s.type = 'text/javascript';
+                s.async = true;
+                s.src = url;
+                var options = {
+                "enabled":true,
+                "chatButtonSetting":{
+                    "backgroundColor":"#00e785",
+                    "ctaText":"Chat with us",
+                    "borderRadius":"25",
+                    "marginLeft": "0",
+                    "marginRight": "20",
+                    "marginBottom": "20",
+                    "ctaIconWATI":false,
+                    "position":"right"
+                },
+                "brandSetting":{
+                    "brandName":"Wati",
+                    "brandSubTitle":"undefined",
+                    "brandImg":"https://www.wati.io/wp-content/uploads/2023/04/Wati-logo.svg",
+                    "welcomeText":"Namashkar! 🙏\nHow can I help you?",
+                    "messageText":"Hi, I'm looking to build a custom software solution for my business. Can you guide me on where to start?",
+                    "backgroundColor":"#00e785",
+                    "ctaText":"Chat with us",
+                    "borderRadius":"25",
+                    "autoShow":false,
+                    "phoneNumber":"+917303942683"
+                }
+                };
+                s.onload = function() {
+                    CreateWhatsappChatWidget(options);
+                };
+                var x = document.getElementsByTagName('script')[0];
+                x.parentNode.insertBefore(s, x);
+            </script>
+
 </body>
 
 
